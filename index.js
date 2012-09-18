c6a2647257368bd9287e95a8b92f68700622e347
Enrico Marino <enrico.marino@email.com>
2012-09-18 20:34:39 +0200

    added matrix2.copy

--- index.js
+++ index.js
@@ -24,6 +24,7 @@
    * create
    * Create a 2d matrix.
    * 
+   * @param {Float32Array} values values
    * @return {Float32Array} 2d matrix
    * @api public
    */
@@ -31,5 +32,26 @@
   matrix2.create = function (values) {
     return new Float32Array(values || [1.0, 0.0, 0.0, 1.0]);
   };
+
+  /**
+   * copy
+   * Copy a 2d matrix.
+   * 
+   * @param {Float32Array} self destination matrix
+   * @param {Float32Array} m source matrix
+   * @return {Float32Array} destination matrix
+   * @api public
+   */
+
+  matrix2.copy = function (self, m) {
+    self[0] = m[0];
+    self[1] = m[1];
+
+    self[2] = m[2];
+    self[3] = m[3];
+
+    return self;
+  };
+
 
 }(this));
